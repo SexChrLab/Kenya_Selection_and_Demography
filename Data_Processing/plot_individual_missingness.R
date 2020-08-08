d704ab76ed663b801c493514545e14cf2f5aa52b
Tanya Phung <tanya.n.phung@gmail.com>
2020-08-07 20:31:31 -0400

    update script

--- Data_Processing/plot_individual_missingness.R
+++ Data_Processing/plot_individual_missingness.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 
+# high and low
 data = data.frame(percentage = c(55.04087, 55.19126, 56.01093, 56.55738, 55.7377, 56.01093, 56.01093, 54.91803, 55.19126, 54.76839, 54.76839, 55.31335, 56.40327, 55.85831, 54.76839, 52.86104, 53.67847, 55.58583, 54.76839, 52.73224, 56.28415, 51.91257), chromosomes = c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"))
 
 data$chromosomes = as.character(data$chromosomes)
@@ -13,3 +14,5 @@ ggplot(data, aes(x=chromosomes, y=percentage)) +
   coord_cartesian(ylim=c(0, 100)) +
   theme(plot.title = element_text(hjust = 0.5))
 dev.off()
+
+# high
